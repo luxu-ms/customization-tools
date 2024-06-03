20b8334af4444a89e1454296f0344646b3ae81c6
Lyle Xu <92287281+luxu-ms@users.noreply.github.com>
2024-06-03 09:51:20 +0800

    Update init-distro.sh

--- wsl/poc/1-bake-image/init-distro.sh
+++ wsl/poc/1-bake-image/init-distro.sh
@@ -22,6 +22,7 @@ appendWindowsPath = false
 
 EOF
 
+# no password for sudo
 cat <<EOF | sudo tee -a /etc/sudoers.d/$username
 $username ALL=(ALL) NOPASSWD:ALL
 EOF
@@ -35,10 +36,6 @@ sudo apt install -y zsh \
     libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
     libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev \
     unzip language-pack-en openjdk-21-jdk-headless
-
-# no password for sudo
-echo "menxiao ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/menxiao
-sudo chmod 0600 /etc/sudoers.d/menxiao
 
 # install nvm for Node.js
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
