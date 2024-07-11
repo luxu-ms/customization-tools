35260519322500e84194527d91f0ccdacc8d81a3
Lyle Xu <luxu@microsoft.com>
2024-07-11 11:40:32 +0800

    try catch for start-process

--- tasks/test-winget/main.ps1
+++ tasks/test-winget/main.ps1
@@ -350,8 +350,16 @@ else {
             $versionFlag = "-Version '$($Version)'"
         }
 
-        $process = Start-Process -FilePath "C:\Program Files\PowerShell\7\pwsh.exe" -Verb RunAs -ErrorAction Stop -NoNewWindow -Wait -PassThru -ArgumentList $($mtaFlag), "-Command `"Install-WinGetPackage -Id '$($Package)' $($versionFlag) | ConvertTo-Json -Depth 10 > $($tempOutFile)`""
-        $installExitCode = $process.ExitCode
+        $installExitCode = -1
+        try { 
+            $process = Start-Process -FilePath "C:\Program Files\PowerShell\7\pwsh.exe" -ErrorAction Stop -NoNewWindow -Wait -PassThru -ArgumentList $($mtaFlag), "-Command `"Install-WinGetPackage -Id '$($Package)' $($versionFlag) | ConvertTo-Json -Depth 10 > $($tempOutFile)`""
+            $installExitCode = $process.ExitCode
+         }
+        catch {
+            Write-Host "###############An error occurred:"
+            Write-Host $_
+        }
+        
         # read the output file and write it to the console
         $unitResults = Get-Content -Path $tempOutFile
         Remove-Item -Path $tempOutFile -Force
